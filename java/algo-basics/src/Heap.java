9ec526f7adbdc8723c43683bedf400c2e0416d7f
charlesxiong <haojun.xiong2010@gmail.com>
2015-02-03 20:17:11 +0800

    堆排序实现

--- java/algo-basics/src/Heap.java
+++ java/algo-basics/src/Heap.java
@@ -1,6 +1,6 @@
 import com.stdlib.*;
 public class Heap {
-	@SuppressWarnings("rawtypes")
+	
 	public static void sort(Comparable[] a){
 		int N=a.length;
 		buildMaxHeap(a);//Stage 1,建立最大堆
@@ -11,14 +11,13 @@ public class Heap {
 	}
 	
 	/*构造堆，不适宜从左向右向堆中添加元素，使用自底向上的方法建堆*/
-	@SuppressWarnings("rawtypes")
 	public static void buildMaxHeap(Comparable[] a) {
 		int N=a.length;
 		for(int i=N/2;i>=1;i--)
 			sink(a,i,N);
 	}
 	
-	@SuppressWarnings("rawtypes")
+	
 	public static void sink(Comparable[] a,int k,int N){
 		while(2*k <= N){
 			int j=2*k;
@@ -30,13 +29,11 @@ public class Heap {
 	}
 	
 	/*堆中标号为i的元素对应数组位置为i-1的元素，堆从1开始算起*/
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static boolean less(Comparable[] a,int i,int j){
 		return a[i-1].compareTo(a[j-1])<0;
 	}
 	
 	/*同上，堆中元素交换分别对应下标小1的数组元素交换*/
-	@SuppressWarnings("rawtypes")
 	public static void swap(Comparable[] a,int i,int j){
 		Comparable temp=a[i-1];
 		a[i-1]=a[j-1];
