da5fccca22ceac4a114bfdbdeee3a77e22b78f98
charlesxiong <haojun.xiong2010@gmail.com>
2015-03-03 23:34:24 +0800

    基于单链表的背包

--- java/algo-basics/src/LinkedBag.java
+++ java/algo-basics/src/LinkedBag.java
@@ -1,25 +1,85 @@
-/**
- * 
- */
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * @author charles
  *
+ *  基于链表的背包实现
  */
-public class LinkedBag {
-
-	/**
-	 * 
-	 */
+public class LinkedBag<Item> implements Iterable<Item>{
+	private Node first;
+	private int N;
+	
+	private class Node {
+		Item item;
+		Node next;
+	}
+	
 	public LinkedBag() {
-		// TODO Auto-generated constructor stub
+		first=null;
+		N=0;
+	}
+	
+	public  boolean isEmpty(){
+		return first==null;
+	}
+	
+	public int size(){
+		return N;
+	}
+	
+	public void add(Item item){
+		Node prev=first;
+		first=new Node();
+		first.item=item;
+		first.next=prev;
+		N++;
+	}
+	
+	
+	@Override
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return new ListIterator();
+	}
+	
+	private class ListIterator implements Iterator<Item>{
+		private Node cur=first;
+		@Override
+		public boolean hasNext() {
+			return cur!=null;
+		}
+
+		@Override
+		public Item next() {
+			 if (!hasNext()) throw new NoSuchElementException();
+	         Item item = cur.item;
+	         cur=cur.next; 
+	         return item;
+			}
+
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+		}
+	}
+	
+	public String toString(){
+		StringBuilder sb=new StringBuilder();
+		for(Item item: this)
+			sb.append(item+" ");
+		return sb.toString();
 	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LinkedBag<String> lb=new LinkedBag();
+		lb.add("hello");
+		lb.add("world");
+		lb.add("i");
+		System.out.println(lb);
 
 	}
 
