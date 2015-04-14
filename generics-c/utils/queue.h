4a00dd428b14bf3bb74550fc969d39ab2f837e86
charlesxiong <haojun.xiong2010@gmail.com>
2015-04-14 23:29:59 +0800

    循环队列头文件

--- generics-c/utils/queue.h
+++ generics-c/utils/queue.h
@@ -1,29 +1,41 @@
 #ifndef _QUEUE_H_
 #define _QUEUE_H_
 
-/*基于动态数组的队列结构定义*/
+#define DEFAULT_CAPACITY 16
+
+/**
+ * 循环队列结构定义
+ * 如果是固定长度的循环数组，一般建议牺牲一个单元来区分队空和队满
+ * 入队时少一个单元，因而一般设定front指向队头的上一个元素，
+ * rear指向队尾元素
+ */
 typedef struct {
-	void *item;
+	void *data;
 	int capacity;
-	int front;
-	int rear;
+	int front; //指向队头元素的上一个元素
+	int rear; //指向队尾元素
 	int type_size;
 } queue_t;
+
 
 /*为队列分配一定容量的内存*/
 queue_t *queue_alloc(int capacity,int type_size);
 queue_t *queue_alloc_default(int type_size);
 
 /*释放队列内存*/
-void queue_free(queue_t *st);
+void queue_free(queue_t *qt);
 
-/*入队: 若队列没有多余的空间，数组容量加倍*/
-void enqueue(queue_t *st,void *item);
+int queue_size(queue_t *qt);
+int queue_empty(queue_t *qt);
+int queue_full(queue_t *qt);
 
-/*出队: 出队列后如果数组太大，将长度减半*/
-void *dequeue(queue_t *st);
+/*入队*/
+void enqueue(queue_t *qt,void *elt);
+
+/*出队*/
+void *dequeue(queue_t *qt);
 
 /*读取队列顶部元素*/
-void *queue_top(queue_t *st);
+void *queue_peek(queue_t *qt);
 
 #endif
