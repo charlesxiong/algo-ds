06a510332f5b92a38007fbaf991d02ae1fda9e0f
charlesxiong <haojun.xiong2010@gmail.com>
2015-04-17 16:54:51 +0800

    不带头结点的单链表实现

--- generics-c/utils/linklist.c
+++ generics-c/utils/linklist.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "linklist.h"
+#include <stdio.h>
 
 /**
- * linklist.c - Linked List Implementation
- *
+ * 非最简的单恋表实现方式，原因：使用不带头结点的单链表
+ * 可比较下使用头结点的双向循环链表实现方法，后者更为简洁!
  */
-
 /*为单链表分配内存*/
 slist_t *slist_alloc(int (*comp)(const void *,const void *)){
 	slist_t *l=malloc(sizeof(slist_t));
@@ -19,7 +17,7 @@ slist_t *slist_alloc(int (*comp)(const void *,const void *)){
 /*释放整个链表的内存*/
 void slist_free(slist_t *l){
 	list_node_t *cur,*tmp;
-	if(l->head){ //头节点不为空
+	if(l->head){ //头指针不为空
 		cur=l->head;
 		while(cur){
 			tmp=cur->next;
@@ -30,7 +28,7 @@ void slist_free(slist_t *l){
 	free(l);
 }
 
-/*在单链表中查找元素，如果存在，返回对应的值，否则返回NULL*/
+/*在单链表中查找元素，如果存在，返回对应的节点，否则返回NULL*/
 void *slist_find(slist_t *l,void *item) {
 	list_node_t *cur;
 	int (*comp)(const void *,const void *);
@@ -38,17 +36,17 @@ void *slist_find(slist_t *l,void *item) {
 	cur=l->head;
 	while(cur){
 		int cmp_res=comp(item,cur->item);
-		if(cmp_res == 0) 
+		if(cmp_res==0) 
 			return cur->item;
 		else
 			cur= cur->next;
 	} 
-	return NULL; //没找到或者头结点为空均返回空
+	return NULL; //没找到或者头指针为空均返回空
 
 }
 
 /*在单链表尾部添加元素*/
-void slist_insert_end(slist_t *l,void *item) {
+void slist_push_back(slist_t *l,void *item) {
 	/*构造新结点*/
 	list_node_t *node=new_list_node(item);
 
@@ -65,15 +63,15 @@ void slist_insert_end(slist_t *l,void *item) {
 }
 
 /*在单链表头部添加元素*/
-void slist_insert_begin(slist_t *l,void *item) {
+void slist_push_front(slist_t *l,void *item) {
 	list_node_t *node=new_list_node(item);
 	node->next=l->head;
-	l->head=node; //无需区分头结点是否为空，情形一样
+	l->head=node; //无需区分头指针是否为空，情形一样
 	l->n++;
 }
 
 /*在单链表尾部删除元素,若存在，返回被删除的元素键值，否则返回NULL*/
-void *slist_delete_end(slist_t *l) {
+void *slist_pop_back(slist_t *l) {
 	list_node_t *cur,*prev;
 	if(l->head){
 		void *res_item;
@@ -100,7 +98,7 @@ void *slist_delete_end(slist_t *l) {
 }
 
 /*在单链表头部删除元素，若存在返回被删除的元素键值，否则返回NULL*/
-void *slist_delete_begin(slist_t *l) {
+void *slist_pop_front(slist_t *l) {
 	list_node_t *cur;
 	if(l->head){
 		cur=l->head;
@@ -116,7 +114,7 @@ void *slist_delete_begin(slist_t *l) {
 }
 
 /*在单链表中找到第一个含item值的节点并删除此节点*/
-void *slist_delete_middle(slist_t *l,void *item) {
+void *slist_delete(slist_t *l,void *item) {
 	list_node_t *cur,*prev;
 	int (*comp)(const void *,const void *);
 	comp=l->comp;
@@ -136,7 +134,7 @@ void *slist_delete_middle(slist_t *l,void *item) {
 			return NULL;
 		} else {
 			if(prev==NULL)
-				return slist_delete_begin(l);
+				return slist_pop_front(l);
 			else {
 				prev->next=cur->next;
 				void *res_item=cur->item;
