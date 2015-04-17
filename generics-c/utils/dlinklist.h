97b6b46e348a4c92f7c6eb1dfbf24b4c98bca754
charlesxiong <haojun.xiong2010@gmail.com>
2015-04-17 16:55:19 +0800

    带头结点的双向循环链表头文件

--- generics-c/utils/dlinklist.h
+++ generics-c/utils/dlinklist.h
@@ -1,6 +1,8 @@
 #ifndef _DLINKLIST_H_
 #define _DLINKLIST_H_
 
+#include <stdlib.h>
+
 /*双向循环链表的结点定义*/
 typedef struct dlist_node {
 	void *item;
@@ -10,10 +12,11 @@ typedef struct dlist_node {
 
 /*双向循环链表的结构定义*/
 typedef struct {
-	dlist_node_t *tail;
+	dlist_node_t *head;
 	int n;
 	int (*comp)(const void *,const void *);
 } dlist_t;
+
 
 /*为双向循环链表分配内存*/
 dlist_t *dlist_alloc(int (*comp)(const void *,const void *)); 
@@ -21,21 +24,40 @@ dlist_t *dlist_alloc(int (*comp)(const void *,const void *));
 /*释放整个链表的内存*/
 void dlist_free(dlist_t *l);
 
-/*在双向循环链表中查找元素，如果存在，返回对应的值，否则返回NULL*/
-void *dlist_find(dlist_t *l,void *item);
+/*双链表的大小*/
+int dlist_size(dlist_t *l);
 
-/*在双向循环链表尾部添加元素*/
-void dlist_insert_end(dlist_t *l,void *item);
+/*遍历访问链表中的每个元素*/
+void dlist_foreach(dlist_t *l,void (*visit)(dlist_node_t *));
+
+/*在双向循环链表中查找元素，如果存在，返回对应的值，否则返回NULL*/
+dlist_node_t *dlist_find(dlist_t *l,void *item);
 
 /*在双向循环链表某位置添加元素*/
-void dlist_insert_middle(dlist_t *l,void *item,int pos);
+void dlist_insert(dlist_t *l,void *item,int pos);
+
+/*在双向循环链表尾部添加元素*/
+void dlist_push_back(dlist_t *l,void *item);
+
+/*在双向链表头部添加元素*/
+void dlist_push_front(dlist_t *l,void *item);
+
+/*在双向循环链表中删除pos位置节点并输出当前值*/
+void *dlist_delete(dlist_t *l,int pos);
 
 /*在双向循环链表尾部删除元素,若存在，返回被删除的元素键值，否则返回NULL*/
-void *dlist_delete_end(dlist_t *l);
+void *dlist_pop_back(dlist_t *l);
 
+/*在双向循环链表头部删除元素，若存在返回被删除的元素键值，否则返回NULL*/
+void *dlist_pop_front(dlist_t *l);
 
-/*在双向循环链表中找到第一个含item值的节点并删除此节点*/
-void *dlist_delete_middle(dlist_t *l,void *item);
+/*创建一个元素节点，让头尾都指向自己并设元素值*/
+static inline dlist_node_t *new_dlist_node(void *item){
+	dlist_node_t *node=malloc(sizeof(dlist_node_t));
+	node->prev=node->next=node;
+	node->item=item;
+	return node;
+}
 
 
 
